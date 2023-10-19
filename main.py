66b8c0802e026723aff2583e7a3a7bbbb907be40
Fblackbelt <fwild1@unh.newhaven.edu>
2023-10-18 23:52:43 -0400

    Midterm

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import csv
 import pprint
 import string
+# Function for getting the information
 def getInformation(Severity):
     URL = "https://services.nvd.nist.gov/rest/json/cves/2.0/"
     data = {
@@ -11,19 +12,23 @@ def getInformation(Severity):
         "cvssV3Severity" : Severity
     }
     response = requests.get(URL, data)
+    # Saves the information to a JSON
     my_json = json.loads(response.text)
     save_file = open("cve_raw.json", "w")
+    #Dumps the JSON information into the JSON file
     json.dump(my_json, save_file, indent = 6)
     save_file.close()
     return
 
-
+# Collects required severity
 Severity = input("Please enter a CVSS V3 severity level to search for {LOW, MEDIUM, HIGH, CRITICAL} \n")
 getInformation(Severity)
 
+# Reads the JSON File
 with open('cve_raw.json') as f:
     myjson = json.load(f)
     f.close()
+#Writes to the CSV file from the JSON
 with open("cve_parsed.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["CVE ID", "Published Date", "Last Modified Date", "Source Identifier", "Base Severity"])
@@ -35,6 +40,7 @@ with open("cve_parsed.csv", "w", newline="") as csvfile:
                         Severity
                         ])
     csvfile.close()
+# Prints the CSV File
 with open("cve_parsed.csv") as csvfile:
     csvreader = csv.reader(csvfile)
     for row in csvreader:
